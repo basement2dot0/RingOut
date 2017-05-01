fa796e97d4694f4b1888cf73a8b3777dd11dd548
along88 <a.long88@yahoo.com>
2017-05-01 17:07:55 -0400

    Initial Camera script created
    ported old YOTD camera script as starting base. Will tweak camera
    behavior further. desire to place a boundary that is padded 2y and 2 x
    within the cameras bounds. when players near this boundary threshold or
    move closer to one another, camera will zoom in or out accordingly

---
+++ RingOutProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform leftTarget;
+    [SerializeField]
+    public Transform rightTarget;
+
+    public float minDistance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceBetweenTargets = Mathf.Abs(leftTarget.position.x - rightTarget.position.x) * 2;
+        float centerPosition = (leftTarget.position.x + rightTarget.position.x) / 2;
+
+        transform.position = new Vector3(
+            centerPosition, transform.position.y,
+            distanceBetweenTargets > minDistance ? -distanceBetweenTargets : -minDistance
+            );
+    }
+}
+
