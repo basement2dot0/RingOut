d3bc9f354cacf928545fc9552cf801b9abc34f04
Alfred Long <a.long88@yahoo.com>
2017-05-20 00:27:03 -0400

    Momentum Bar
    damage types implemented
    momentumbar added

---
+++ RingOutProject/Assets/MomentumBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MomentumBar : MonoBehaviour
+{
+  
+    private Slider momentumBar;
+    [SerializeField]
+    private bool isPlayerOne; 
+    private void Awake()
+    {
+        momentumBar = GameObject.FindGameObjectWithTag("Slider").GetComponent<Slider>();
+        
+    }
+
+    public void OnHit(float amount)
+    {
+        if (isPlayerOne)
+        {
+            momentumBar.value += Mathf.Lerp(momentumBar.value, momentumBar.minValue,amount);
+
+
+        }
+        else
+        {
+            momentumBar.value -= Mathf.Lerp(momentumBar.value, momentumBar.maxValue, amount);
+        }
+    }
+}
