0b4156a4618dc96b9a8e212e38e80ff3a2c896dc
DESKTOP-AP3B9BV\Joshmond <joshmnd8@gmail.com>
2017-05-26 16:14:18 +0100

    Implemented combo system.
    Test Combo = Jump, Jump, Jump.
    
    Player One Jump: Left Sihft.
    
    Player Two Jump: Space

---
+++ RingOutProject/Assets/Combo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combo : MonoBehaviour
+{
+
+    private ComboSystem Hadouken;
+    private InputManager inputManager;
+
+    private void Awake()
+    {
+        inputManager = GetComponent<InputManager>();
+
+        Hadouken = new ComboSystem(new string[] { "Jump" + inputManager.controlNo , "Jump" + inputManager.controlNo, "Jump" + inputManager.controlNo});
+    }
+
+    void Update ()
+    {
+        if (Hadouken.CheckCombo())
+        {
+            Debug.Log(inputManager.controlNo + " HADOUKEN!");
+        }
+	}
+}
