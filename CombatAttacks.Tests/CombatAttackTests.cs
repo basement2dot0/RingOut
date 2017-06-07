848822537df139624fd2ce2cb90503ba8567a97a
Alfred Long <a.long88@yahoo.com>
2017-06-07 19:00:54 -0400

    TDD testing implementation

---
+++ CombatAttacks.Tests/CombatAttackTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CombatAttacks.Tests
+{
+    [TestFixture]
+    public class ComboAttacksTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
