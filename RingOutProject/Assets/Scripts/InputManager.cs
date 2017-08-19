dbdcff6eaeabf85f37afef3afd9d3ed6c80913b5
Alfred Long <a.long88@yahoo.com>
2017-08-19 00:52:45 -0400

    Revert "Ro 99 marieonett"

---
+++ RingOutProject/Assets/Scripts/InputManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager instance;
+    public static InputManager Instance
+    {
+        get
+        {
+          if(instance == null)
+                instance = new InputManager();
+            return instance;
+        }
+    }
+
+    public float GetHorizontal(){
+        return Input.GetAxisRaw("Horizontal");}
+
+    public float GetVertical(){
+        return Input.GetAxisRaw("Vertical");}
+
+    public Vector3 Movement(){
+        return new Vector3(GetHorizontal(), 0, GetVertical());}
+
+    public bool AttackButtonDown(){
+        return Input.GetButtonDown("Fire1");}
+
+    public bool AttackButtonUP(){
+        return Input.GetButtonUp("Fire1");}
+
+    public bool DefendButtonDown(){
+        return Input.GetButtonDown("Fire2");}
+
+    public bool DefendButtonUp(){
+        return Input.GetButtonUp("Fire2");}
+
+    public bool GrabButtonDown(){
+        return Input.GetButtonDown("Jump");}
+
+    public bool GrabButtonUp(){
+        return Input.GetButtonUp("Fire3");}
+
+
+}
