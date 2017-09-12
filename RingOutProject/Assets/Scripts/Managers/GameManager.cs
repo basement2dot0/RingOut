f2a592e712aa077a459ab6f970dd33a4a3165a0c
Alfred Long <a.long88@yahoo.com>
2017-09-12 16:33:30 -0400

    RO-447: added UI elements and game objects to game scene. Added additional pause menu ui logic and flow to GameManager.Cs currently unable to allow selecting of either menu button while game is paused

--- RingOutProject/Assets/Scripts/Managers/GameManager.cs
+++ RingOutProject/Assets/Scripts/Managers/GameManager.cs
@@ -14,14 +14,27 @@ public class GameManager : MonoBehaviour
     private Player[] players;
     [SerializeField]
     private Text uiText;
-    [SerializeField]
-    private InputManager inputManager;
-    private bool isPaused;
 
+    private Button[] pauseButtons;
+    private Button quit;
+    private bool isPaused;
+    private GameObject pauseMenuObject;
+    private GameObject nav;
     private void Awake()
     {
         uiText = GetComponentInChildren<Text>();
-        inputManager = GetComponent<HumanInput>();
+        pauseMenuObject = GameObject.FindGameObjectWithTag("ShowOnPause");
+        nav = GameObject.FindGameObjectWithTag("Nav");
+        pauseButtons = new Button[2];
+        foreach (Button button in pauseMenuObject.GetComponentsInChildren<Button>())
+        {
+            if (button.name.ToLower() == string.Format("resume"))
+                pauseButtons[0] = button;
+            else
+                pauseButtons[1] = button;
+        }
+        pauseMenuObject.SetActive(false);
+        nav.transform.position = (pauseButtons[0].transform.position - new Vector3(100, 0, 0));
     }
 
     private void Start()
@@ -49,33 +62,38 @@ public class GameManager : MonoBehaviour
     
     private void Update()
     {
-        Debug.Log(isPaused.ToString());
+       
         PauseMenu();
-       if (players[0].IsHypeHit|| players[1].IsHypeHit)
-            uiText.text = "RING OUT!";
-
+        RingOutVictory();
+       
 
     }
 
+    private void FixedUpdate()
+    {
+ 
 
+    }
 
     private void PauseMenu()
     {
-        if (inputManager.PauseButton())
+        if (PauseButton())
         {
            if(!isPaused)
             {
+                
                 Time.timeScale = 0.0f;
-                uiText.text = string.Format("PAUSE");
+                //uiText.text = string.Format("PAUSE");
                 isPaused = true;
-                Debug.Log("PAUSED");
+                pauseMenuObject.SetActive(true);
+                StartCoroutine("Counter");
             }
             else if(isPaused)
             {
                 Time.timeScale = 1.0f;
-                uiText.text = "";
+                //uiText.text = "";
                 isPaused = false;
-                Debug.Log("UNPAUSED");
+                pauseMenuObject.SetActive(false);
             }
         }
     }
@@ -84,6 +102,62 @@ public class GameManager : MonoBehaviour
     {
         if (players[0].IsHypeHit || players[1].IsHypeHit)
             uiText.text = "RING OUT!";
+    }
+
+    private void PauseControls()
+    {
+            var resumeButton = (pauseButtons[0].transform.position - new Vector3(100, 0, 0));
+            var quitButton = (pauseButtons[1].transform.position - new Vector3(100, 0, 0));
+         
+
+        if (Navigation() > 0.0f)
+            {
+                Debug.Log("Resume");
+            
+                nav.transform.position = resumeButton;
+            
+            }
+            if (Navigation() < 0.0f)
+            {
+                Debug.Log("Quit");
+                nav.transform.position = quitButton ;
+            }
+            if (ConfirmButton())
+            {
+              
+            }
+
+    }
+
+
+    private bool PauseButton()
+    {
+        return Input.GetButtonDown("Submit");
+    }
+    private float Navigation()
+    {
+        return Input.GetAxis("Vertical1");
+    }
+    private bool ConfirmButton()
+    {
+        return Input.GetButtonDown("Attack1");
+    }
+
+
+    //Testing
+    IEnumerator Counter()
+    {
+        while (true)
+        {
+            float pauseEndTime = Time.realtimeSinceStartup + 1f;
+            while (Time.realtimeSinceStartup < pauseEndTime)
+            {
+                
+                yield return 0;
+                PauseControls();
+            }
+           
+        }
     }
 }
 
