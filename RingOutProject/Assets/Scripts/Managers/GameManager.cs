267d784a3e9a0f585cf1f4a66df46c8bc8c08001
Alfred Long <a.long88@yahoo.com>
2017-09-13 00:09:07 -0400

    RO-447: Cleaned up code to conform to coding standards and practices

--- RingOutProject/Assets/Scripts/Managers/GameManager.cs
+++ RingOutProject/Assets/Scripts/Managers/GameManager.cs
@@ -6,11 +6,10 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    //this class should be loaded with the stage in an empty game object
     [SerializeField]
-    private float Rounds;
+    private float rounds;
     [SerializeField]
-    private float Match;
+    private float match;
     [SerializeField]
     private Player[] players;
     [SerializeField]
@@ -21,9 +20,9 @@ public class GameManager : MonoBehaviour
     private bool isPaused;
     private GameObject pauseMenuObject;
     private GameObject nav;
+
     private void Awake()
     {
-        
         pauseMenuObject = GameObject.FindGameObjectWithTag("ShowOnPause");
         nav = GameObject.FindGameObjectWithTag("Nav");
         pauseButtons = new Button[2];
@@ -37,43 +36,23 @@ public class GameManager : MonoBehaviour
         pauseMenuObject.SetActive(false);
         nav.transform.position = (pauseButtons[0].transform.position - new Vector3(100, 0, 0));
     }
-
     private void Start()
     {
         players = new Player[2];
         foreach (var player in GameObject.FindGameObjectsWithTag("Player"))
         {
             if (player.GetComponent<Player>().ID == 1)
-            {
                 players[0] = player.GetComponent<Player>();
-
-
-            }
-
             else
-            {
                 players[1] = player.GetComponent<Player>();
-
-            }
-
+            rounds = 0;
+            uiText.text = "";
         }
-        Rounds = 0;
-        uiText.text = "";
     }
-    
     private void Update()
     {
-       
         PauseMenu();
         RingOutVictory();
-       
-
-    }
-
-    private void FixedUpdate()
-    {
- 
-
     }
 
     private void PauseMenu()
@@ -82,10 +61,7 @@ public class GameManager : MonoBehaviour
         {
            if(!isPaused)
             {
-              uiText =  pauseMenuObject.GetComponentInChildren<Text>();
-                uiText.text = "PAUSED";
                 Time.timeScale = 0.0f;
-                //uiText.text = string.Format("PAUSE");
                 isPaused = true;
                 pauseMenuObject.SetActive(true);
                 StartCoroutine("PauseNavigation");
@@ -93,7 +69,6 @@ public class GameManager : MonoBehaviour
             
         }
     }
-
     private void RingOutVictory()
     {
         if (players[0].IsHypeHit || players[1].IsHypeHit)
@@ -103,61 +78,42 @@ public class GameManager : MonoBehaviour
         }
             
     }
-
     private void PauseControls()
     {
             var resumeButton = (pauseButtons[0].transform.position - new Vector3(100, 0, 0));
             var quitButton = (pauseButtons[1].transform.position - new Vector3(100, 0, 0));
          
 
-        if (Navigation() > 0.0f)
-            {
-                Debug.Log("Resume");
-            
+            if (Navigation() > 0.0f)
                 nav.transform.position = resumeButton;
-            
-            }
             if (Navigation() < 0.0f)
-            {
-                Debug.Log("Quit");
                 nav.transform.position = quitButton ;
-            }
             if (ConfirmButton())
             {
-              if(nav.transform.position == quitButton)
-              {
-                
-                SceneManager.LoadScene("Main Menu");
-                
-              }
-              if(nav.transform.position == resumeButton)
-            {
-                Time.timeScale = 1.0f;
-                //uiText.text = "";
-                isPaused = false;
-                pauseMenuObject.SetActive(false);
-
+                if(nav.transform.position == quitButton)
+                    SceneManager.LoadScene("Main Menu");
+                if(nav.transform.position == resumeButton)
+                {
+                    Time.timeScale = 1.0f;
+                    isPaused = false;
+                    pauseMenuObject.SetActive(false);
+                }
             }
-        }
-
     }
-
 
     private bool PauseButton()
     {
         return Input.GetButtonDown("Submit");
     }
-    private float Navigation()
-    {
-        return Input.GetAxis("Vertical1");
-    }
     private bool ConfirmButton()
     {
         return Input.GetButtonDown("Attack1");
     }
-
-
-    //Testing
+    private float Navigation()
+    {
+        return Input.GetAxis("Vertical1");
+    }
+    
     IEnumerator PauseNavigation()
     {
         while (true)
