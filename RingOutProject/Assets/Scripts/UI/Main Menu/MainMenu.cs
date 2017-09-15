127295e018f4244762dbabc4acc4e2833549c5f6
DESKTOP-85GJ6NR\Joshmond <joshmnd8@gmail.com>
2017-09-15 01:05:43 +0100

    Implemented main menu functionality for RO-461

---
+++ RingOutProject/Assets/Scripts/UI/Main Menu/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
